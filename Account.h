08e80fec1bc549b25c25403805d87d7ad6b6f99a
LiamGetHub <lgfitting@gmail.com>
2025-02-22 11:45:51 -0800

    created abstract Account class, started bank class but will finish later once finer business logic is flushed out

---
+++ Account.h
@@ -0,0 +1,45 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include <iostream>
+#include "User.h"
+
+using namespace std;
+
+/*
+* The idea of this class is to make it an unistantiable (abstract class) 
+* boilerplate for future acounts such as checkings, savings, or investment.
+* The child classes should overide 
+*/
+class Account {
+    public:
+        // Pure virtual function (forces derived classes to implement)
+        virtual void deposit(double amount) = 0;
+        virtual bool withdraw(double amount) = 0; // returns boolean value to indicate sucsess. Important to check for edge cases.
+        virtual void calculateIntrest() = 0;
+
+        double getBalence() {
+            return this->balence;
+        }
+
+        Account(){
+            accountID = -1;
+            userPtr->setUserID(-1);
+            acctName = "UKNOWN";
+            double balence = 0;
+        }
+
+        virtual ~Account(){};
+
+        
+    protected:
+        int accountID;
+        User * userPtr;
+        double intrestRate;
+        string acctName;
+        double balence;
+};
+
+
+
+#endif
---
+++ Bank.h
@@ -0,0 +1,27 @@
+#ifndef BANK_H
+#define BANK_h
+
+#include <vector>
+#include <iostream>
+#include "User.H"
+
+using namespace std;
+
+class Bank {
+    public:
+
+
+    private:
+        vector<User> users; // vector that holds the users
+        string bankName;
+        double intrestRate;
+        double totalDeposites;
+        // array of loans
+        // 
+
+    protected:
+
+};
+
+
+#endif
